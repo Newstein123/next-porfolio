43d4d5e93ec25930be2d452adf5a51bf4b1b6dc7
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2024-03-17 23:12:41 +0630

    production error fixed

--- app/componets/project/ProjectCardInfo.jsx
+++ app/componets/project/ProjectCardInfo.jsx
@@ -9,16 +9,16 @@ import Link from "next/link";
 
 const ProjectCardInfo = ({item}) => {
   return (
-    <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-violet-700 dark:border-violet-700">
+    <div className="w-full max-w-md p-4  border rounded-lg shadow sm:p-8 bg-violet-700 border-violet-700">
       <div className="flex items-center justify-between mb-4">
-        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+        <h5 className="text-xl font-bold leading-nonetext-white">
           Project Information
         </h5>
       </div>
       <div className="flow-root overflow-hidden">
         <ul
           role="list"
-          className="divide-y divide-gray-200 dark:divide-gray-700"
+          className="divide-y divide-gray-200divide-gray-700"
         >
           {/* client info */}
           <li className="py-3 sm:py-4">
@@ -27,10 +27,10 @@ const ProjectCardInfo = ({item}) => {
                 <IoPerson size={25} />
               </div>
               <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p className="text-sm font-medium text-white truncate">
                   Client
                 </p>
-                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                <p className="text-sm text-gray-400 truncate ">
                   {item.client}
                 </p>
               </div>
@@ -43,10 +43,10 @@ const ProjectCardInfo = ({item}) => {
                 <FaLayerGroup size={25} />
               </div>
               <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p className="text-sm font-medium  truncate text-white">
                   Category
                 </p>
-                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                <p className="text-sm  truncate text-gray-400">
                   {item.category}
                 </p>
               </div>
@@ -59,10 +59,10 @@ const ProjectCardInfo = ({item}) => {
                 <BsCalendarDate size={25} />
               </div>
               <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p className="text-sm font-medium truncate  text-white">
                   Date
                 </p>
-                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                <p className="text-sm truncate text-gray-400">
                   {item.date}
                 </p>
               </div>
@@ -75,10 +75,10 @@ const ProjectCardInfo = ({item}) => {
                 <MdLocationPin size={25} />
               </div>
               <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p className="text-sm font-medium truncate text-white">
                   Location
                 </p>
-                <p className="text-sm text-gray-500 dark:text-gray-400">
+                <p className="text-sm text-gray-400">
                   {item.location}
                 </p>
               </div>
@@ -91,14 +91,14 @@ const ProjectCardInfo = ({item}) => {
                 <IoIosGlobe size={25} />
               </div>
               <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p className="text-sm font-medium truncate text-white">
                   Website Url
                 </p>
                 {item.website_url !== "" ? (
                   <Link
                     href={item?.website_url ?? "#"}
                     target="_blank"
-                    className="text-sm text-gray-500 dark:text-gray-400 underline hover:text-blue-500"
+                    className="text-sm text-gray-400 underline hover:text-blue-500"
                   >
                     {item.website_url}
                   </Link>
@@ -115,14 +115,14 @@ const ProjectCardInfo = ({item}) => {
                 <FaGithub size={25} />
               </div>
               <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <p className="text-sm font-medium truncate text-white">
                   Github Url
                 </p>
                 {item.gitlink !== "" ? (
                   <Link
                     href={item?.gitlink ?? "#"}
                     target="_blank"
-                    className="text-sm text-gray-500 dark:text-gray-400 underline hover:text-blue-400"
+                    className="text-sm text-gray-400 underline hover:text-blue-400"
                   >
                     {item.gitlink}
                   </Link>
